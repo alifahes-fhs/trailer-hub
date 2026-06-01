8d48b2d0418f14b47274a9194d8631d74e840c96
Ali Fahes <alibfahes@gmail.com>
2026-06-02 01:50:33 +0300

    Add Firebase config

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey:            "AIzaSyBRIT0-DjvyeMjeU-tvYlet3Z_ngLgLW4E",
+  authDomain:        "trailer-hub-fd68c.firebaseapp.com",
+  projectId:         "trailer-hub-fd68c",
+  storageBucket:     "trailer-hub-fd68c.firebasestorage.app",
+  messagingSenderId: "778104528195",
+  appId:             "1:778104528195:web:5d7581127f9b9697f163f6"
+};
+firebase.initializeApp(firebaseConfig);
